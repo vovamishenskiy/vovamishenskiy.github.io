5ab6fbfe92f0e52a2fa7a316b34d9776962f6045
andrey_bb <andreimuzhevsky@gmail.com>
2022-04-03 16:35:25 +0400

    added more adaptivity

--- script.js
+++ script.js
@@ -11,4 +11,6 @@ document.addEventListener("DOMContentLoaded", function(){
   document.getElementsByClassName("socketWiki")[0].addEventListener("click",function(){
     window.location.href = "https://ru.wikipedia.org/wiki/%D0%A1%D0%BF%D0%B8%D1%81%D0%BE%D0%BA_%D1%80%D0%B0%D0%B7%D1%8A%D1%91%D0%BC%D0%BE%D0%B2_%D0%BC%D0%B8%D0%BA%D1%80%D0%BE%D0%BF%D1%80%D0%BE%D1%86%D0%B5%D1%81%D1%81%D0%BE%D1%80%D0%BE%D0%B2";
   });
+  var cpuLink = getComputedStyle(document.getElementsByClassName("cpuWiki")[0]);
+  console.log(cpuLink.href);
 });
